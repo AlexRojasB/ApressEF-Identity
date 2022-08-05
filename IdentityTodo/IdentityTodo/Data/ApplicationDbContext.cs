d00dd41ca3d6c2995c02d47544579884d45f59ce
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-08-04 20:09:29 -0600

    first commit

---
+++ IdentityTodo/IdentityTodo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityTodo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityTodo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
